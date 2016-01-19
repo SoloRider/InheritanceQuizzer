75f216c27a4b5580882ef6509d4ad2b6f8029dc2
christopher.ferguson <christopher.ferguson@MU-C1-06.mullen.pvt.k12.co.us>
2016-01-19 14:47:24 -0700

    12 more questions

--- InheritanceQuizzer/src/InheritanceRunner.java
+++ InheritanceQuizzer/src/InheritanceRunner.java
@@ -5,12 +5,16 @@ public class InheritanceRunner
 		static int correct = 0;
 		public static void main(String[] args)
 			{
+				System.out.println("Bird b1 = new Bird()");
+				System.out.println("Bird b2 = new Wren()");
+				System.out.println("Wren w1 = new Bird()");
+				System.out.println("Wren w2 = new Wren()");
 				//Question 1
 				System.out.println("Here are your questions.");
 				System.out.println("Would Bird b2 = new Wren(); throw an error at you?");
 				Scanner user1nput = new Scanner(System.in);
 				String choice = user1nput.nextLine();
-				if(choice.equals("no") || choice.equals("No"))
+				if(choice.equals("No") || choice.equals("no"))
 					{
 						System.out.println("That is correct.");
 						correct++;
@@ -46,32 +50,120 @@ public class InheritanceRunner
 				System.out.println("Next question.");
 				System.out.println("Referring to the question above, would x1.runAMethod work?");
 				String choice3 = user1nput.nextLine();
-				if(choice3.equals("Yes") || choice3.equals("yes"))
+				if(choice3.equals("No") || choice3.equals("no"))
 					{
 						System.out.println("You are correct.");
 						correct++;
 					}
 				else
 					System.out.println("That is wrong.");
-//				Bird b1 = new Bird( );
-//				Bird b2 = new Wren( );
-//				Wren w1 = new Bird( );
-//				Wren w2 = new Wren( );
-//				b1.BirdMethod1( );
-//				b1.BirdMethod2( );
-//				b1.WrenMethod1( );
-//				b1.WrenMethod2( );
-//				b2.BirdMethod1( );
-//				b2.BirdMethod2( );
-//				b2.WrenMethod1( );
-//				b2.WrenMethod2( );
-//				w1.BirdMethod1( );
-//				w1.BirdMethod2( );
-//				w1.WrenMethod1( );
-//				w1.WrenMethod2( );
-//				w2.BirdMethod1( );
-//				w2.BirdMethod2( );
-//				w2.WrenMethod1( );
-//				w2.WrenMethod2( );
+				//Question 5
+				System.out.println();
+				System.out.println("Next question.");
+				System.out.println("If Dog and Cat extend Animal and Lion extends Cat, would Lion.runAnimalMethod() work?");
+				String choice4 = user1nput.nextLine();
+				if(choice4.equals("Yes") || choice4.equals("yes"))
+					{
+						System.out.println("That is correct.");
+						correct++;
+					}
+				else
+					System.out.println("That is wrong.");
+				//Question 6
+				System.out.println();
+				System.out.println("Next question.");
+				System.out.println("If Lambo and Ferrari extend NiceCars and LamboElemento extends Lambo, would Lambo l1 = new NiceCars(); work?");
+				String choice5 = user1nput.nextLine();
+				if(choice5.equals("No") || choice5.equals("no"))
+					{
+						System.out.println("That is correct.");
+						correct++;
+					}
+				else
+					System.out.println("That is wrong.");
+				//Question 7
+				System.out.println();
+				System.out.println("Next question.");
+				System.out.println("If Lambo and Ferrari extend NiceCars and LamboElemento extends Lambo, would LamboElemento.runFerrariMethod(); work?");
+				String choice6 = user1nput.nextLine();
+				if(choice6.equals("No") || choice6.equals("no"))
+					{
+						System.out.println("That is correct.");
+						correct++;
+					}
+				else
+					System.out.println("That is wrong.");
+				//Question 8
+				System.out.println();
+				System.out.println("Next question.");
+				System.out.println("If Lambo and Ferrari extend NiceCars and LamboElemento extends Lambo, would NiceCars.runFerrariMethod(); work?");
+				String choice7 = user1nput.nextLine();
+				if(choice7.equals("No") || choice7.equals("no"))
+					{
+						System.out.println("That is correct.");
+						correct++;
+					}
+				else
+					System.out.println("That is wrong.");
+				//Question 9
+				System.out.println();
+				System.out.println("Next question.");
+				System.out.println("If Lambo and Ferrari extend NiceCars and LamboElemento extends Lambo, would Lambo.runFerrariMethod(); work?");
+				String choice8 = user1nput.nextLine();
+				if(choice8.equals("No") || choice8.equals("no"))
+					{
+						System.out.println("That is correct.");
+						correct++;
+					}
+				else
+					System.out.println("That is wrong.");
+				//Question 10
+				System.out.println();
+				System.out.println("Next question.");
+				System.out.println("Referring to the previous question, would that run with super?");
+				String choice9 = user1nput.nextLine();
+				if(choice9.equals("Yes") || choice9.equals("Yes"))
+					{
+						System.out.println("That is correct.");
+						correct++;
+					}
+				else
+					System.out.println("That is wrong.");
+				//Question 11
+				System.out.println();
+				System.out.println("Next question.");
+				System.out.println("If Lambo and Ferrari extend NiceCars and LamboElemento extends Lambo, would NiceCars.runFerrariMethod(); work?");
+				String choice10 = user1nput.nextLine();
+				if(choice10.equals("No") || choice10.equals("no"))
+					{
+						System.out.println("That is correct.");
+						correct++;
+					}
+				else
+					System.out.println("That is wrong.");
+				//Question 12
+				System.out.println();
+				System.out.println("Next question.");
+				System.out.println("Adding a new one under Ferrari named Italia, would Italia.runFerrariMethod() work?");
+				String choice11 = user1nput.nextLine();
+				if(choice11.equals("Yes") || choice11.equals("yes"))
+					{
+						System.out.println("That is correct.");
+						correct++;
+					}
+				else
+					System.out.println("That is wrong.");
+				//Question 13
+				System.out.println();
+				System.out.println("Next question.");
+				System.out.println("If Lambo and Ferrari extend NiceCars and LamboElemento extends Lambo, would LamboElemento.runFerrariMethod(); work?");
+				String choice12 = user1nput.nextLine();
+				if(choice12.equals("No") || choice12.equals("no"))
+					{
+						System.out.println("That is correct.");
+						correct++;
+					}
+				else
+					System.out.println("That is wrong.");
 			}
 	}
